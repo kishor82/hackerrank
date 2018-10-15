3dd18aba283dfe99aa8def9e4d95cce8098d15c2
kishor <kishorrathva8298@gmail.com>
2018-10-16 00:43:38 +0530

    Solution

---
+++ generic/generic.java
@@ -0,0 +1,40 @@
+//Generics are for compile-time-safety 
+//The good programming strategy says it is far better to handle the 
+//problem at compiletime than runtime.
+
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer
+{
+   public <E> void printArray(E[] elements){
+       for(E element:elements){
+           System.out.println(element);
+       }
+   }
+ 
+}
+
+public class Solution {
+
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
+    }
+}
